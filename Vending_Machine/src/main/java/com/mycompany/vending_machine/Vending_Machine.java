acf25722dc536209241b9d8f2d3458c747ee5c59
SomeGuyEatingPie <seandavidjohnson132@gmail.com>
2023-03-05 17:43:08 +0000

    Setup
    Created basic java swing interface

---
+++ Vending_Machine/src/main/java/com/mycompany/vending_machine/Vending_Machine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vending_machine;
+
+/**
+ *
+ * @author seand
+ */
+public class Vending_Machine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
